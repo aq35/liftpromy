f3c6d932762a64bd99dedde06776f6a3d9667861
kazuma hosobe <32251518+aq35@users.noreply.github.com>
2024-06-21 08:06:32 +0900

    デプロイ(プロフィール叩き台)

--- liftpromy/src/App.vue
+++ liftpromy/src/App.vue
@@ -1,20 +1,14 @@
 <template>
   <div id="app">
-    <Header />
     <router-view />
-    <Footer />
   </div>
 </template>
 
 <script>
 import 'normalize.css'
-import Header from './components/Header.vue';
-import Footer from './components/Footer.vue';
 
 export default {
   components: {
-    Header,
-    Footer
   }
 };
 </script>
--- liftpromy/src/components/Profile.vue
+++ liftpromy/src/components/Profile.vue
@@ -4,14 +4,17 @@
         <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
             <a class="navbar-brand js-scroll-trigger" href="#page-top">
                 <span class="d-block d-lg-none">細部 寿麻</span>
-                <span class="d-none d-lg-block"><img class="img-fluid img-profile rounded-circle mx-auto mb-2" src=""
-                        alt="..." /></span>
+                <span class="d-none d-lg-block">
+                    <img class="img-fluid img-profile rounded-circle mx-auto mb-2" src="@assets/aq35/logo.webp"
+                        :alt="'プロフィール'" />
+                </span>
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive"
                 aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span
                     class="navbar-toggler-icon"></span></button>
             <div class="collapse navbar-collapse" id="navbarResponsive">
                 <ul class="navbar-nav">
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#profile">PROFILE</a></li>
                     <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#about">About</a></li>
                     <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#experience">Experience</a></li>
                     <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#education">Education</a></li>
@@ -23,6 +26,60 @@
         </nav>
         <!-- Page Content-->
         <div class="container-fluid p-0">
+            <!-- プロフィール -->
+            <section class="resume-section" id="profile">
+
+                <div class="resume-section-content">
+                    <h1 class="mb-0">
+                        PRO<span class="text-primary">FILE</span>
+                    </h1>
+                    <div class="card mb-3 mx-auto" style="max-width: 1200px;">
+                        <div class="card-body">
+                            <h5 class="card-title">細部 寿麻 (ホソベ カズマ)</h5>
+                            <p class="card-text">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                    class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
+                                    <path
+                                        d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z">
+                                    </path>
+                                </svg> 秋田県秋田市下浜八田
+                            </p>
+                            <p class="card-text">
+                                こんにちは、<strong>細部
+                                    寿麻</strong>と申します。現在、システム開発会社で主にWebシステムの開発を担当しています。お客様のニーズに応じた最適なソリューションを提供することに情熱を注いでいます。
+                            </p>
+                            <p class="card-text">
+                                子どものころは、なまはげ文化が色濃く残る秋田県秋田市下浜八田で育ちました。この地域の独特な文化や風習に触れながら、地域の温かさと自然の美しさに囲まれて成長しました。
+                            </p>
+                            <p class="card-text">
+                                社会人としてのキャリアの初めの5年間は、工場やスーパーのレジなど、さまざまな職種でのアルバイトを経験しました。これらの経験を通じて、異なる業界の業務フローやお客様対応のスキルを学びました。
+                            </p>
+                            <p class="card-text">
+                                その後、エンジニアSIerとして東京都日本橋で某与信系システムの開発に1年間携わり、開発やテストの基礎を身につけました。また、埼玉県所沢にあるグループホームで介護士（初任者研修）として1年間勤務し、福祉分野での貴重な経験を積みました。
+                            </p>
+                            <p class="card-text">
+                                現在のシステム開発会社に入社してからは5年目になります。ここでは、Webシステムの設計から開発、運用まで一貫して担当し、プロジェクトの成功に貢献しています。
+                            </p>
+                            <p class="card-text">
+                                プライベートでは、技術の進歩に遅れないよう常に学習を続けています。最近では、業務で使用するDockerの知識をさらに深め、kubernetesの学習にも取り組んでいます。これにより、より効率的でスケーラブルなシステムを構築するスキルを磨いています。
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+
+                <!-- <div class="resume-section-content">
+                    <h2 class="mb-5">PROFILE</h2>
+                    <p>Apart from being a web developer, I enjoy most of my time being outdoors. In the winter, I am an
+                        avid skier and novice ice climber. During the warmer months here in Colorado, I enjoy mountain
+                        biking, free climbing, and kayaking.</p>
+                    <p class="mb-0">When forced indoors, I follow a number of sci-fi and fantasy genre movies and
+                        television shows, I am an aspiring chef, and I spend a large amount of my free time exploring
+                        the latest technology advancements in the front-end web development world.</p>
+                </div> -->
+            </section>
+            <hr class="m-0" />
+
             <!-- About-->
             <section class="resume-section" id="about">
                 <div class="resume-section-content">
