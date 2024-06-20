727f32256e1e0e177cf7a0e16f1b7a378a2517b9
kazuma hosobe <32251518+aq35@users.noreply.github.com>
2024-06-20 22:38:20 +0900

    Initial commit

--- liftpromy/vite.config.ts
+++ liftpromy/vite.config.ts
@@ -4,11 +4,4 @@ import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     plugins: [vue()],
-    css: {
-        preprocessorOptions: {
-            scss: {
-                additionalData: `@import "@/styles/main.scss";`
-            }
-        }
-    }
 });
